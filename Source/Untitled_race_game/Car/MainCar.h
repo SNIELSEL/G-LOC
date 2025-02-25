0ffd326686037f92bdced531df90e283ad5d97c2
SNIELSEL <niels@haverkotte.nl>
2025-02-25 16:57:57 +0100

    ue5 and vs compile settings and car script

---
+++ Source/Untitled_race_game/Car/MainCar.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WheeledVehiclePawn.h"
+#include "MainCar.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNTITLED_RACE_GAME_API AMainCar : public AWheeledVehiclePawn
+{
+	GENERATED_BODY()
+	
+};
