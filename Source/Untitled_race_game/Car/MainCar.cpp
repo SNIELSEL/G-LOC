76963f58d801d549a1c6047a3871cdc787be981e
SNIELSEL <niels@haverkotte.nl>
2025-02-28 06:53:49 +0100

    car physics and input detection

--- Source/Untitled_race_game/Car/MainCar.cpp
+++ Source/Untitled_race_game/Car/MainCar.cpp
@@ -2,4 +2,62 @@
 
 
 #include "MainCar.h"
+#include "GameFrameWork/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
+AMainCar::AMainCar()
+{
+	SpringArmC = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArmC->SetupAttachment(RootComponent);
+	SpringArmC->TargetArmLength = 600;
+	SpringArmC->SetRelativeLocation(FVector(0, 0, 140));
+	SpringArmC->SetRelativeRotation(FRotator(-10, 0, 0));
+	CameraC = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	CameraC->SetupAttachment(SpringArmC);
+}
+
+void AMainCar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(FName("MoveForward"), this, &AMainCar::Accelerate);
+	PlayerInputComponent->BindAxis(FName("MoveBackward"), this, &AMainCar::Decelerate);
+	PlayerInputComponent->BindAxis(FName("MoveLR"), this, &AMainCar::MoveHorizontal);
+	PlayerInputComponent->BindAxis(FName("LookUD"), this, &AMainCar::LookVertical);
+	PlayerInputComponent->BindAxis(FName("LookLR"), this, &AMainCar::LookHorizontal);
+
+	PlayerInputComponent->BindAction(FName("Brake"), EInputEvent::IE_Pressed, this, &AMainCar::BrakePressed);
+	PlayerInputComponent->BindAction(FName("Brake"), EInputEvent::IE_Released, this, &AMainCar::BrakeReleased);
+
+}
+
+void AMainCar::Accelerate(float value)
+{
+
+}
+void AMainCar::Decelerate(float value)
+{
+
+}
+void AMainCar::MoveHorizontal(float value)
+{
+
+}	
+void AMainCar::BrakePressed()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Brake pressed"));
+}
+void AMainCar::BrakeReleased()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Brake released"));
+}
+
+//look
+void AMainCar::LookVertical(float value)
+{
+
+}
+void AMainCar::LookHorizontal(float value)
+{
+
+}
--- Source/Untitled_race_game/Car/MainCar.h
+++ Source/Untitled_race_game/Car/MainCar.h
@@ -6,12 +6,35 @@
 #include "WheeledVehiclePawn.h"
 #include "MainCar.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class UNTITLED_RACE_GAME_API AMainCar : public AWheeledVehiclePawn
 {
 	GENERATED_BODY()
 	
+public:
+
+	AMainCar();
+
+	UPROPERTY(BluePrintReadWrite, EditAnywhere)
+		class USpringArmComponent* SpringArmC;
+	UPROPERTY(BluePrintReadWrite, EditAnywhere)
+		class UCameraComponent* CameraC;
+protected:
+	virtual void SetupPlayerInputComponent(UInputComponent * PlayerInputComponent) override;
+private:
+
+	//Movement
+	void Accelerate(float value);
+	void Decelerate(float value);
+	void MoveHorizontal(float value);
+	void BrakePressed();
+	void BrakeReleased();
+
+	//Look
+	void LookVertical(float value);
+	void LookHorizontal(float value);
+
+	//End Movement
+
 };
